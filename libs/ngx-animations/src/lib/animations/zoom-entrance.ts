b94c4f7454c9aafd56bd9f6f1545c8b0a7d8b95b
LAPTOP-D8PKKVPH\Ismail <ismail.benjelloun@gmail.com>
2018-09-04 22:23:54 +0200

    Fix zoom animations

--- libs/ngx-animations/src/lib/animations/zoom-entrance.ts
+++ libs/ngx-animations/src/lib/animations/zoom-entrance.ts
@@ -4,7 +4,7 @@ export function zoomIn(time: number): (AnimationStyleMetadata | AnimationAnimate
   const t = time;
   return [
     style({  opacity: '0', transform: 'scale3d(0.3, 0.3, 0.3)' }),
-    animate(t, style({ opacity: '1', transform: 'rotate3d(0, 0, 1, 80deg)', offset: .5 })),
+    animate(t, style({ opacity: '1', transform: 'scale3d(1, 1, 1)' })),
   ];
 }
 
@@ -12,7 +12,7 @@ export function zoomInDown(time: number): (AnimationStyleMetadata | AnimationAni
   const t = time;
   return [
     style({  opacity: '0', transform: 'scale3d(0.1, 0.1, 0.1) translate3d(0, -1000px, 0)', 'animation-timing-function': 'cubic-bezier(0.55, 0.055, 0.675, 0.19)' }),
-    animate(t, style({ opacity: '1', transform: 'scale3d(0.475, 0.475, 0.475) translate3d(0, 60px, 0)', 'animation-timing-function': 'cubic-bezier(0.175, 0.885, 0.32, 1)', offset: .6 })),
+    animate(t, style({ opacity: '1', transform: 'scale3d(1, 1, 1) translate3d(0, 0, 0)', 'animation-timing-function': 'cubic-bezier(0.175, 0.885, 0.32, 1)' })),
   ];
 }
 
@@ -20,7 +20,7 @@ export function zoomInLeft(time: number): (AnimationStyleMetadata | AnimationAni
   const t = time;
   return [
     style({  opacity: '0', transform: 'scale3d(0.1, 0.1, 0.1) translate3d(-1000px, 0, 0)', 'animation-timing-function': 'cubic-bezier(0.55, 0.055, 0.675, 0.19)' }),
-    animate(t, style({ opacity: '1', transform: 'scale3d(0.475, 0.475, 0.475) translate3d(10px, 0, 0)', 'animation-timing-function': 'cubic-bezier(0.175, 0.885, 0.32, 1)', offset: .6 })),
+    animate(t, style({ opacity: '1', transform: 'scale3d(1, 1, 1) translate3d(0, 0, 0)', 'animation-timing-function': 'cubic-bezier(0.175, 0.885, 0.32, 1)' })),
   ];
 }
 
@@ -28,7 +28,7 @@ export function zoomInRight(time: number): (AnimationStyleMetadata | AnimationAn
   const t = time;
   return [
     style({  opacity: '0', transform: 'scale3d(0.1, 0.1, 0.1) translate3d(1000px, 0, 0)', 'animation-timing-function': 'cubic-bezier(0.55, 0.055, 0.675, 0.19)' }),
-    animate(t, style({ opacity: '1', transform: 'scale3d(0.475, 0.475, 0.475) translate3d(-10px, 0, 0)', 'animation-timing-function': 'cubic-bezier(0.175, 0.885, 0.32, 1)', offset: .6 })),
+    animate(t, style({ opacity: '1', transform: 'scale3d(1, 1, 1) translate3d(0, 0, 0)', 'animation-timing-function': 'cubic-bezier(0.175, 0.885, 0.32, 1)' })),
   ];
 }
 
@@ -36,6 +36,6 @@ export function zoomInUp(time: number): (AnimationStyleMetadata | AnimationAnima
   const t = time;
   return [
     style({  opacity: '0', transform: 'scale3d(0.1, 0.1, 0.1) translate3d(0, 1000px, 0)', 'animation-timing-function': 'cubic-bezier(0.55, 0.055, 0.675, 0.19)' }),
-    animate(t, style({ opacity: '1', transform: 'scale3d(0.475, 0.475, 0.475) translate3d(0, -60px, 0)', 'animation-timing-function': 'cubic-bezier(0.175, 0.885, 0.32, 1)', offset: .6 })),
+    animate(t, style({ opacity: '1', transform: 'scale3d(1, 1, 1) translate3d(0, 0, 0)', 'animation-timing-function': 'cubic-bezier(0.175, 0.885, 0.32, 1)' })),
   ];
 }
--- libs/ngx-animations/src/lib/animations/zoom-exit.ts
+++ libs/ngx-animations/src/lib/animations/zoom-exit.ts
@@ -13,7 +13,7 @@ export function zoomOutDown(time: number): (AnimationStyleMetadata | AnimationAn
   const t = time;
   return [
     style({ }),
-    animate(t, style({ opacity: '0', transform: 'scale3d(0.475, 0.475, 0.475) translate3d(0, -60px, 0)', 'animation-timing-function': 'cubic-bezier(0.55, 0.055, 0.675, 0.19)' , offset: .4 })),
+    animate(t, style({ opacity: '0.5', transform: 'scale3d(0.475, 0.475, 0.475) translate3d(0, 0, 0)', 'animation-timing-function': 'cubic-bezier(0.55, 0.055, 0.675, 0.19)' , offset: .4 })),
     animate(t, style({ opacity: '0', 'transform-origin': 'center bottom', transform: 'scale3d(0.1, 0.1, 0.1) translate3d(0, 2000px, 0)', 'animation-timing-function': 'cubic-bezier(0.175, 0.885, 0.32, 1)' })),
   ];
 }
@@ -22,7 +22,7 @@ export function zoomOutLeft(time: number): (AnimationStyleMetadata | AnimationAn
   const t = time;
   return [
     style({ }),
-    animate(t, style({ opacity: '0', transform: 'scale3d(0.475, 0.475, 0.475) translate3d(42px, 0, 0)' , offset: .4 })),
+    animate(t, style({ opacity: '0.5', transform: 'scale3d(0.475, 0.475, 0.475) translate3d(42px, 0, 0)' , offset: .4 })),
     animate(t, style({ opacity: '0', 'transform-origin': 'left center', transform: 'scale(0.1) translate3d(-2000px, 0, 0)' })),
   ];
 }
@@ -31,7 +31,7 @@ export function zoomOutRight(time: number): (AnimationStyleMetadata | AnimationA
   const t = time;
   return [
     style({ }),
-    animate(t, style({ opacity: '0', transform: 'scale3d(0.475, 0.475, 0.475) translate3d(-42px, 0, 0)' , offset: .4 })),
+    animate(t, style({ opacity: '0.5', transform: 'scale3d(0.475, 0.475, 0.475) translate3d(-42px, 0, 0)' , offset: .4 })),
     animate(t, style({ opacity: '0', 'transform-origin': 'right center', transform: 'scale(0.1) translate3d(2000px, 0, 0)' })),
   ];
 }
@@ -40,7 +40,7 @@ export function zoomOutUp(time: number): (AnimationStyleMetadata | AnimationAnim
   const t = time;
   return [
     style({ }),
-    animate(t, style({ opacity: '0', transform: 'scale3d(0.475, 0.475, 0.475) translate3d(0, 60px, 0)', 'animation-timing-function': 'cubic-bezier(0.55, 0.055, 0.675, 0.19)' , offset: .4 })),
+    animate(t, style({ opacity: '0.5', transform: 'scale3d(0.475, 0.475, 0.475) translate3d(0, 60px, 0)', 'animation-timing-function': 'cubic-bezier(0.55, 0.055, 0.675, 0.19)' , offset: .4 })),
     animate(t, style({ opacity: '0', 'transform-origin': 'center bottom', transform: 'scale3d(0.1, 0.1, 0.1) translate3d(0, -2000px, 0)', 'animation-timing-function': 'cubic-bezier(0.175, 0.885, 0.32, 1)' })),
   ];
 }
