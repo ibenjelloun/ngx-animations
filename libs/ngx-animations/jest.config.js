e46373bbd1afdc91fe3417ad170970a9797f2c35
LAPTOP-D8PKKVPH\Ismail <ismail.benjelloun@gmail.com>
2019-02-20 22:15:21 +0100

    migrating unit tests from karma to jest

---
+++ libs/ngx-animations/jest.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  name: 'ngx-animations',
+  preset: '../../jest.config.js',
+  coverageDirectory: '../../coverage/libs/ngx-animations'
+};
--- libs/ngx-animations/src/lib/services/animations.service.spec.ts
+++ libs/ngx-animations/src/lib/services/animations.service.spec.ts
@@ -2,26 +2,21 @@ import { AnimationsService } from './animations.service';
 import { animations } from '../model/animations-list';
 
 describe('AnimationsService (with spies)', () => {
-  let animationBuilderSpy: { build: jasmine.Spy };
-  let animationFactorySpy: { create: jasmine.Spy };
+  const animationFactory = { create: (any) => {} };
+  const animationBuilder = { build: (any) => animationFactory };
+  let animationBuilderSpy;
+  let animationFactorySpy;
   let animationsService: AnimationsService;
 
   beforeEach(() => {
-    animationBuilderSpy = jasmine.createSpyObj('AnimationBuilder', ['build']);
-    animationFactorySpy = jasmine.createSpyObj('AnimationFactory', ['create']);
-    animationBuilderSpy.build.and.returnValue(animationFactorySpy);
-    animationsService = new AnimationsService(<any>animationBuilderSpy);
+    animationBuilderSpy = jest.spyOn(animationBuilder, 'build');
+    animationFactorySpy = jest.spyOn(animationFactory, 'create');
+    animationsService = new AnimationsService(<any>animationBuilder);
   });
 
   it('should return animation player', () => {
     animationsService.create(animations.bounce(300), null);
-    expect(animationBuilderSpy.build.calls.count()).toBe(
-      1,
-      'AnimationBuilder.build 1 call'
-    );
-    expect(animationFactorySpy.create.calls.count()).toBe(
-      1,
-      'AnimationFactory.create 1 call'
-    );
+    expect(animationBuilderSpy).toHaveBeenCalledTimes(1);
+    expect(animationFactorySpy).toHaveBeenCalledTimes(1);
   });
 });
