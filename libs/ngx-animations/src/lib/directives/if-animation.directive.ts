c0ee6af5924741bb5a28a39462a46b66810ef4d5
ibenjelloun <ismail.benjelloun@orange.com>
2020-02-11 15:49:17 +0100

    Fix animIf initialisation error using DoCheck hook

--- libs/ngx-animations/src/lib/directives/if-animation.directive.ts
+++ libs/ngx-animations/src/lib/directives/if-animation.directive.ts
@@ -1,15 +1,16 @@
-import { Directive, Input, ViewContainerRef, TemplateRef, EmbeddedViewRef } from '@angular/core';
+import { Directive, Input, ViewContainerRef, TemplateRef, EmbeddedViewRef, DoCheck } from '@angular/core';
 import { AnimationsService } from '../services/animations.service';
 import { timer } from 'rxjs';
 
 @Directive({
   selector: '[animIf]'
 })
-export class AnimIfDirective {
-  @Input()
-  animIfInfo: { startAnim: string; endAnim: string; time: number };
+export class AnimIfDirective implements DoCheck {
+  @Input() animIfInfo: { startAnim: string; endAnim: string; time: number };
   shownBefore = false;
   subscription;
+  show;
+  _animIfDirty;
 
   constructor(
     private templateRef: TemplateRef<any>,
@@ -19,21 +20,32 @@ export class AnimIfDirective {
 
   @Input()
   set animIf(show: boolean) {
-    if (this.subscription) {
-      this.subscription.unsubscribe();
-    }
-    if (show && !this.shownBefore) {
-      this.viewContainer.clear();
-      this.viewContainer.createEmbeddedView(this.templateRef);
-      const player = this.createPlayer(this.animIfInfo.startAnim, this.animIfInfo.time);
-      player.play();
-      this.shownBefore = true;
-    }
-    if (!show && this.shownBefore) {
-      const player = this.createPlayer(this.animIfInfo.endAnim, this.animIfInfo.time);
-      player.play();
-      this.clearAfterTime(this.viewContainer, this.animIfInfo.time);
-      this.shownBefore = false;
+    this.show = show;
+    this._animIfDirty = true;
+  }
+
+  /**
+   * Applies the changes when needed.
+   */
+  ngDoCheck() {
+    if (this._animIfDirty) {
+      this._animIfDirty = false;
+      if (this.subscription) {
+        this.subscription.unsubscribe();
+      }
+      if (this.show && !this.shownBefore) {
+        this.viewContainer.clear();
+        this.viewContainer.createEmbeddedView(this.templateRef);
+        const player = this.createPlayer(this.animIfInfo.startAnim, this.animIfInfo.time);
+        player.play();
+        this.shownBefore = true;
+      }
+      if (!this.show && this.shownBefore) {
+        const player = this.createPlayer(this.animIfInfo.endAnim, this.animIfInfo.time);
+        player.play();
+        this.clearAfterTime(this.viewContainer, this.animIfInfo.time);
+        this.shownBefore = false;
+      }
     }
   }
 
